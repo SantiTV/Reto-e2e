57684fd3ffd868c916f5724a9203be1dc2a9d4ff
SantiTV <s.tenjov@uniandes.edu.co>
2025-04-06 22:40:41 -0500

    arreglo prueba login

--- cypress/e2e/integration/login.cy.js
+++ cypress/e2e/integration/login.cy.js
@@ -2,11 +2,19 @@ describe('Login en Pinterest', () => {
     beforeEach(() => {
         cy.session('login', () => {
             cy.visit('https://www.pinterest.com/login/');
-            cy.get('input[name="id"]').type('santtv76@gmail.com ');
+            cy.get('input[name="id"]').type('santtv76@gmail.com');
             cy.get('input[name="password"]').type('Pinterest123');
             cy.get('button[type="submit"]').click();
             cy.url().should('include', '/homefeed');
         });
     });
 
+    it('Debe ingresar correctamente y mostrar el homefeed', () => {
+        // Cypress reusará la sesión "login"
+        cy.visit('https://www.pinterest.com/');
+        cy.url().should('include', '/homefeed');
+
+        // Validación opcional: buscar un elemento visible del homefeed
+        cy.contains('Para ti').should('exist'); // Cambia según el idioma
+    });
 });
